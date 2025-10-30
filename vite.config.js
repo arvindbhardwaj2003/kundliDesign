70c41c8bfc0201019f1951fccfee64fdc059437e
arvindbhardwaj2003 <arvindbhardwaj2003@gmail.com>
2025-10-30 22:25:17 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend',
+  root: './frontend',
   build: {
     outDir: '../dist',
     emptyOutDir: true
+  },
+  server: {
+    host: true,
+    port: 5173
   }
 })
